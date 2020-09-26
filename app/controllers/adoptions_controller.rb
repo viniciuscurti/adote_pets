d3942f43e4f8cd4724631beff4ef6683c21448a0
vinicius curti <vinicius_c_l@hotmail.com>
2020-09-26 18:36:54 -0300

    Add dotenv - Protect my secret data in .env file

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,2 +1,19 @@
 class AdoptionsController < ApplicationController
+  def create
+    @pet = Pet.find(params[:pet_id])
+    @adoption = Adoption.new(adoption_params)
+    @adoption.pet = @pet
+
+    if @adoption.save
+      redirect_to animal_path(@pet)
+    else
+      render 'pets/show'
+    end
+  end
+
+  private
+
+  def adoption_params
+    params.require(:adoption).permit(:owner_name, :pet_id)
+  end
 end
