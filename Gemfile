7f2fcde7b472d4819d1b95186086cd8f7886c4bc
vinicius curti <vinicius_c_l@hotmail.com>
2020-09-28 16:36:29 -0300

    cloudinary teste

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'simple_form'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'dotenv-rails'
 gem 'cloudinary'
+gem 'activestorage-cloudinary-service'
 gem 'friendly_id', '~> 5.4.0'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_214909) do
+ActiveRecord::Schema.define(version: 2020_09_28_192137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,17 @@ ActiveRecord::Schema.define(version: 2020_09_26_214909) do
     t.index ["pet_id"], name: "index_adoptions_on_pet_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "category"
@@ -51,6 +62,8 @@ ActiveRecord::Schema.define(version: 2020_09_26_214909) do
     t.boolean "adopted"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_pets_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
