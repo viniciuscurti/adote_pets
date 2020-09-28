620c55bdc6ff863b955529908087f579f2178ddc
vinicius curti <vinicius_c_l@hotmail.com>
2020-09-28 17:40:57 -0300

    cloudinary ainda sem funcionar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+Pet.destroy_all
+
+puts 'Start'
+
+5.times do
+  file = URI.open('https://source.unsplash.com/300x300/?cat')
+  pet = Pet.new(
+                      name: Faker::Creature::Cat.name.downcase,
+                      birth: Faker::Date.between(from: '2014-09-23', to: '2019-09-25'),
+                      category: 'cat')
+  pet.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  pet.save!
+end
+
+5.times do
+  file = URI.open('https://source.unsplash.com/300x300/?dog')
+  pet = Pet.new(
+                      name: Faker::Creature::Dog.name.downcase,
+                      birth: Faker::Date.between(from: '2014-09-23', to: '2019-09-25'),
+                      category: 'dog')
+  pet.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  pet.save!
+end
+
+puts 'Finish'
